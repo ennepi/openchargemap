fd6d5eced7c3fd5093884e99796881d9afd39de2
Nicola Pesce <44465677+ennepi@users.noreply.github.com>
2019-01-21 12:39:46 +0100

    Add files via upload
    Creazione mappa leaflet con funzione di geolocalizzazione.

---
+++ js/map.js
@@ -0,0 +1,54 @@
+var map;
+var marker = null;
+var a = null;
+var b = null;
+
+
+function load_map() {
+
+
+    map = L.map('mapid').setView([41.9055688, 12.4659426], 13);
+
+    $('.leaflet-top').removeClass('leaflet-top').addClass('leaflet-bottom');
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibmtwIiwiYSI6ImNqcXBjajg1aDAwNmw0M24ya3Y3azgyb2YifQ.6VAH3nW-oDBjiXAb-KfoKg', {
+        maxZoom: 18,
+        id: 'mapbox.streets'
+    }).addTo(map);
+
+
+}
+
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+        if (marker !== null) {
+            map.removeLayer(marker)
+        }
+   
+        
+
+    }
+}
+
+function showPosition(position) {
+    map.panTo(L.latLng(position.coords.latitude, position.coords.longitude));
+
+    if (marker !== null) {
+        map.removeLayer(marker)
+    }
+
+    var myIcon = L.icon({
+        iconUrl: 'https://image.flaticon.com/icons/svg/708/708971.svg',
+        iconSize: [38, 95],
+        iconAnchor: [22, 73],
+    });
+
+    marker = L.marker([position.coords.latitude, position.coords.longitude],{icon: myIcon}).addTo(map);
+
+     
+
+}
+
+window.onload = load_map;
