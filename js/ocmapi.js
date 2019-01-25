43afdff9de04bf31b2e8dcf33753c6ff4d6ae837
Nicola Pesce <44465677+ennepi@users.noreply.github.com>
2019-01-25 19:25:22 +0100

    Add files via upload
    Aggiunta creazione del percorso attraverso Leaflet Routing Machine

--- js/ocmapi.js
+++ js/ocmapi.js
@@ -15,4 +15,41 @@ $.getJSON("https://api.myjson.com/bins/cqa18", function showPie(UsageCost) {
         }
         
     }
+
+    tot= valoremaggiore + valoreminore;
+    valoremaggiore= valoremaggiore/tot*100;
+    valoremaggiore= Math.round(valoremaggiore);
+    valoreminore= valoreminore/tot*100;
+    valoreminore= Math.round(valoreminore);
+
+
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var chart = new Chart(ctx, {
+      
+        type: 'doughnut',
+    
+       
+        data: {
+            labels: ["0,40 €KWh", "Free"],
+            datasets: [{
+                label: "Rome Usage Cost",
+                backgroundColor: [
+                    'rgb(0,200,83)',
+                    'rgb(165,214,167)'
+                ],
+                borderColor: 'rgb(255,255,255)',
+                data: [valoremaggiore, valoreminore],
+                
+            }]
+        },
+    
+      
+        options: {
+            legend: {
+               position: 'left'
+            },
+        }
+    });
+    
+
 });
